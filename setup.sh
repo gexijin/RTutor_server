a2829d0df104f51b20c045ba0b29557159590556
Steven Ge <xijin.ge@sdstate.edu>
2022-12-11 20:10:01 -0600

    scripts shell

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
 sudo docker build ./nginx/. -t nginx --pull #nginx image should be build very quick
 sudo docker build . -t webapp --pull #webapp image need hours to build
-sudo docker-compose down
-sudo docker-compose up -d --scale webapp=2
