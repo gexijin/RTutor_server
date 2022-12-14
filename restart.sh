22787b5805cf6847d5cd6ebad46d5dd73a3b81da
Steven Ge <xijin.ge@sdstate.edu>
2022-12-14 03:57:52 -0600

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,3 @@
+echo $(date) >> /home/ubuntu/log.txt
 sudo docker-compose -f /home/ubuntu/RTutor_server/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/RTutor_server/docker-compose.yml up -d --scale webapp=20
