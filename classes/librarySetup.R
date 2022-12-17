82667c29006b4720fadd626933ed1fd2b7a0166b
Steven Ge <xijin.ge@sdstate.edu>
2022-12-16 21:00:08 -0600

    Add top 5000 packages from CRAN

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -77,6 +77,47 @@ list_packages <- c(
 
   #--Data visualization--
 )
-install.packages(list_packages)
+#install.packages(list_packages)
 
 remotes::install_github("gexijin/RTutor", upgrade = "never")
+
+# Install top 5000 packages from CRAN
+
+# list of all available packages
+all <- available.packages()
+all <- as.vector(all[, 1])
+installed <- .packages(all.available = TRUE)
+new_pacakge <- setdiff(all, installed)
+
+# get download statistics for all CRAN packages
+remotes::install_github("r-hub/cranlogs")
+
+dls <- sapply(
+  new_pacakge, #[1:1000], 
+  function(p) {
+    sum(cranlogs::cran_downloads(
+      package = p, 
+      from = "2022-01-01", 
+      to = "2022-12-15"
+    )$count)
+  }
+)
+
+
+# Rank
+dls <- sort(dls, decreasing = TRUE)
+top <- 5000
+
+missedPackages <- names(dls[1:top])
+
+for (i in 1:length(missedPackages))
+{
+  pkgName <- missedPackages[i]
+  install.packages(
+    pkgName, 
+    upgrade = "never",
+    quiet = TRUE,
+    Ncpus = 2
+  )
+}
+print("END")
